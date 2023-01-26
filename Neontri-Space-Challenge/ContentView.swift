c3d84e49bdceafde1adf33bd1d626c2d36ba41d8
Sebastian Worek <sebastian1.worek@gmail.com>
2023-01-26 10:37:11 +0100

    Initial Commit

---
+++ Neontri-Space-Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Neontri-Space-Challenge
+//
+//  Created by Sebastian on 26/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neontri-Space-Challenge/Neontri_Space_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neontri_Space_ChallengeApp.swift
+//  Neontri-Space-Challenge
+//
+//  Created by Sebastian on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Neontri_Space_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
