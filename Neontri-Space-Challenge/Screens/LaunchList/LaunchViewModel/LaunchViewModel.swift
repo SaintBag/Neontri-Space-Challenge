71f0125026d5dcbfe8a8c8b1ab839986cb4faa92
Sebastian Worek <sebastian1.worek@gmail.com>
2023-01-27 16:28:26 +0100

    move ApiService to separated folder

--- Neontri-Space-Challenge/Screens/LaunchList/LaunchViewModel/LaunchViewModel.swift
+++ Neontri-Space-Challenge/Screens/LaunchList/LaunchViewModel/LaunchViewModel.swift
@@ -7,39 +7,6 @@
 
 import Foundation
 
-protocol LaunchFetchingProtocol {
-    func fetchLaunches() async throws -> [Launch]
-}
-
-class ApiService: LaunchFetchingProtocol {
-    
-    func fetchLaunches() async throws -> [Launch] {
-        let launchUrlString = "https://ll.thespacedevs.com/2.2.0/launch/upcoming/"
-        guard let url = URL(string: launchUrlString) else {
-            throw LaunchError.invalidUrl
-        }
-        
-        let urlRequest = URLRequest(url: url)
-        let (data, response) = try await URLSession.shared.data(for: urlRequest)
-        if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
-            throw LaunchError.invalidStatusCode(statusCode: statusCode)
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            let response = try decoder.decode(LaunchesResponse.self, from: data)
-            print(response)
-            return response.results
-            
-        } catch {
-            print(data)
-            print("problem to decode JSON", error)
-            throw LaunchError.failedToDecode(error: error)
-        }
-    }
-}
-
 @MainActor
 class LaunchViewModel: ObservableObject {
     @Published var launches: [Launch] = []
---
+++ Neontri-Space-Challenge/Service/ApiService.swift
@@ -0,0 +1,41 @@
+//
+//  ApiService.swift
+//  Neontri-Space-Challenge
+//
+//  Created by Sebastian on 27/01/2023.
+//
+
+import Foundation
+
+protocol LaunchFetchingProtocol {
+    func fetchLaunches() async throws -> [Launch]
+}
+
+class ApiService: LaunchFetchingProtocol {
+    
+    func fetchLaunches() async throws -> [Launch] {
+        let launchUrlString = "https://ll.thespacedevs.com/2.2.0/launch/upcoming/"
+        guard let url = URL(string: launchUrlString) else {
+            throw LaunchError.invalidUrl
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
+            throw LaunchError.invalidStatusCode(statusCode: statusCode)
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            let response = try decoder.decode(LaunchesResponse.self, from: data)
+            print(response)
+            return response.results
+            
+        } catch {
+            print(data)
+            print("problem to decode JSON", error)
+            throw LaunchError.failedToDecode(error: error)
+        }
+    }
+}
