dcbfd521a8913471397b507a7c007704ef51e343
SaintBag <99802688+SaintBag@users.noreply.github.com>
2023-01-27 13:00:18 +0100

    add URLImage SPM, add proper image in placeholderView (#4)

--- Neontri-Space-Challenge/Screens/LaunchList/ImagePlaceholderView.swift
+++ Neontri-Space-Challenge/Screens/LaunchList/ImagePlaceholderView.swift
@@ -6,19 +6,37 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct ImagePlaceholderView: View {
+    let imageString: String?
+    let id: String?
+    
     var body: some View {
-        Image(systemName: "person.fill")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .clipShape(Circle())
-            .overlay(Circle().stroke(Color.blue, lineWidth: 10))
+        if let imageUrl = imageString,
+           let url = URL(string: imageUrl) {
+            URLImage(url, identifier: id, failure: { error, _ in
+                
+            }, content: { rocketImage in
+                rocketImage
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.blue, lineWidth: 10))
+            })
+        } else {
+            Image(systemName: "person.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.blue, lineWidth: 10))
+        }
     }
 }
 
 struct ImagePlaceholderView_Previews: PreviewProvider {
     static var previews: some View {
-        ImagePlaceholderView()
+        ImagePlaceholderView(imageString: "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launch_images/falcon2520925_image_20221214103909.png", id: "1")
     }
 }
